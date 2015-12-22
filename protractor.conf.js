739f1d22a2e9e9451cd92c5c48b40f374f5276e9
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-12-22 12:38:10 -0600

    Resolves #31

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 //Protractor configuration file.
 exports.config = {
     //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
     capabilities: {
         'browserName': 'chrome'
     },
