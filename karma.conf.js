c0db996d82c1bce9e1cfa011cf7fe9b10110a7be
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-12-30 21:24:31 -0600

    Attempts to get travis working

--- karma.conf.js
+++ karma.conf.js
@@ -3,27 +3,17 @@ module.exports = function(config) {
     var configuration = {
         frameworks: ['jasmine'],
         files: [
-          'app/**/*.js',
-          'app/**/*.spec.js'
+            'app/**/*.js',
+            'app/**/*.spec.js'
         ],
         exclude: [
-          'app/lib/**/*'
+            'app/lib/**/*'
         ],
         reporters: ["spec"],
         autoWatch: false,
-        browsers: ['Chrome'],
         singleRun: true,
-        customLaunchers: {
-          Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox', '--javascript-harmony']
-          }
-        }
+        browsers: ['Firefox']
     };
-    
-    if(process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
-    }
     
     config.set(configuration);
 }
